110d2db1b910994f0edac71e31ea9803f8206b14
jaclynflomen <j_floman@fanshaweonline.ca>
2019-03-13 13:30:09 -0400

    please work

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+    name: "Jaclyn",
+    role: "student",
+
+    user1:{
+        name:"user",
+        role:"developer",
+        bio: "some bio info goes here",
+        bioPic: ""
+    },
+
+    user2:{
+        name:"user2",
+        role:"designer",
+        bio: "some bio info goes here",
+        bioPic: ""
+    }
+}
